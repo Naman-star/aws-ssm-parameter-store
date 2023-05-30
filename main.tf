cea8168cf338e9936268d3d3a2173ab17801d474
user <namanshetty91132@gmail.com>
2023-05-30 15:40:34 +0100

    aws-ssm-parameter-store

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
+//  type  = "String"
   value = "Hello World"
+  type = "SecureString"
+  key_id = "8ed6ee73-145a-4993-ba10-87a6c6bd693b"
 }
