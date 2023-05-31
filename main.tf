2ea2842a49e80f404639f2904abcab1fd3eaa32d
user <namanshetty91132@gmail.com>
2023-05-31 03:47:53 +0100

    aws-ssm-parameter-store

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
-//  type  = "String"
+#  type  = "String"
   value = "var.parameters[count.index].value
   type = "String"
   key_id = "8ed6ee73-145a-4993-ba10-87a6c6bd693b"
