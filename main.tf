8b94eed520cfdfa87ea6c67182a30d4cd2003394
user <namanshetty91132@gmail.com>
2023-06-04 14:09:36 +0100

    aws-ssm-parameter-store

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ resource "aws_ssm_parameter" "parameters" {
   type = "String"
   key_id = "8ed6ee73-145a-4993-ba10-87a6c6bd693b"
 }
+
+#Passwords are not at all meant to be keep inside the code at all, Here since it is a lab we are doing it to save time.
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type = "SecureString"
+  key_id = "8ed6ee73-145a-4993-ba10-87a6c6bd693b"
+}
